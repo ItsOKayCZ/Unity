88a12c6245f2a88393732b683688f7f218e1dbe8
ItsOKayCZ <oldrich.krupauer@gmail.com>
2020-11-12 09:06:38 +0100

    Adding projects

---
+++ 2020/War of caves/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    //Movement
+    public float camSpeed = 10f;
+    public float camBoarderT = 10f;
+    public Vector2 camLimitor;
+    
+    //Zoom
+    public float zoomSpeed = 100f;
+    public float maxY = 20f;
+    public float minY = 20f; 
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("up") || Input.mousePosition.y  >= Screen.height -camBoarderT)
+        {
+            pos.z += camSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("down") || Input.mousePosition.y <= camBoarderT)
+        {
+            pos.z -= camSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("left") || Input.mousePosition.x <= camBoarderT)
+        {
+            pos.x -= camSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("right") || Input.mousePosition.x >= Screen.width - camBoarderT)
+        {
+            pos.x += camSpeed * Time.deltaTime;
+        }
+
+        float zoom = Input.GetAxis("Mouse ScrollWheel");
+        pos.y -= zoom * zoomSpeed *1000f *  Time.deltaTime;
+
+        //Clamp movement
+        pos.x = Mathf.Clamp(pos.x, -camLimitor.x, camLimitor.x);
+        pos.z = Mathf.Clamp(pos.z, -camLimitor.y, camLimitor.y);
+
+        //Clamp zoom
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+        transform.position = pos;
+    }
+}
---
+++ 2020/War of caves/Assets/Scripts/Map.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    public float width;
+    public float height;
+
+    public float gridWidth;
+
+    private List<List<GameObject>> grid = new List<List<GameObject>>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < width; i++){
+
+            grid.Add(new List<GameObject>());
+            for(int j = 0; j < height; j++){
+                GameObject square = createSquare(i + 1, j + 1);
+
+                Instantiate(square, this.transform);
+                grid[i].Add(square);
+            }
+        }
+    }
+
+    /**
+     * Creates 1 square of the grid
+     */
+    GameObject createSquare(float x, float y){
+        GameObject square = new GameObject("Grid X: " + x.ToString() + " Y: " + y.ToString());
+        square.hideFlags = HideFlags.HideInHierarchy;
+        square.transform.localScale = new Vector3(gridWidth, 1, gridWidth);
+
+        square.transform.position = new Vector3(x * (gridWidth * 10), 0, y * (gridWidth * 10));
+
+        return square;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
